14eb8ae85e06ec81ea07c7ced85a1b47e9a7ea2c
lamine-f <mouhamedlaminefaye77@gmail.com>
2024-02-07 23:57:28 +0000

    adding search-bar.component.tsx

--- src/app/_components/search-bar/search-bar.component.tsx
+++ src/app/_components/search-bar/search-bar.component.tsx
@@ -2,87 +2,16 @@
 import styles from "./search-bar.module.css"
 import React, {useEffect, useState} from "react";
 import Link from "next/link";
-export default function NavBar () {
+export default function SearchBar () {
 
-  const [isMobile, setIsMobile] = useState<boolean>(window!.innerWidth <= 425);
 
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-        setIsMobile(window.innerWidth <= 425);
-    })
-  }, []);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-      {
-        isMobile ?
-          <BurgerMenu/>
-          :
-            <Buttons
-            >
-              <Button
-                active={false}
-                href={"/"}
-                children={"Acceuil"}
-              />
-              <Button
-                href={"concert"}
-                active={false}
-                children={"Concert"}
-              />
-            </Buttons>
-      }
+
       </div>
     </div>
   )
 }
 
-
-function Buttons ({children}:{children: React.ReactNode[]}) {
-  return (
-    <>{children}</>
-  )
-}
-
-function Button ({active, children, href}:{
-  active: boolean,
-  children: string | React.ReactNode,
-  href: string
-}) {
-  return (
-    <Link href={href}  className={styles.text}>
-      {children}
-    </Link>
-  )
-}
-
-export const BurgerMenu = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <div className={styles.burgerMenu}>
-      <div className={`${styles.icon} ${isOpen ? styles.open : ''}`} onClick={handleToggle}>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
-      </div>
-      {isOpen && (
-        <div className={styles.menu}>
-
-          <div className={styles.menuItemContainer} >
-            <Link className={styles.menuItem} onClick={() => handleToggle()} href={"home"}>Acceuil</Link>
-          </div>
-
-          <div className={styles.menuItemContainer} >
-            <Link className={styles.menuItem} onClick={() => handleToggle()} href={"concert"}>Concert</Link>
-          </div>
-
-        </div>
-      )}
-    </div>
-  );
-};
